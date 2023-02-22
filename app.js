c8dcc5dee3c50b5168ef1b89a05d241d74f05822
roodhouse <rughjm@gmail.com>
2023-02-22 13:23:58 -0600

    show repos function

--- app.js
+++ app.js
@@ -16,16 +16,19 @@ searchUser.addEventListener('keyup', (e) => {
         // Make http call
         github.getUser(userText)
             .then(data => {
-                if(data.profile.message === 'Not found') {
+                if(data.profile.message === 'Not Found') {
                     // show alert
+                    ui.showAlert('User not found', 'alert alert-danger');
 
                 } else {
                     // show profile
                     ui.showProfile(data.profile);
+                    ui.showRepos(data.repos)
                 }
             })
     } else {
         // clear profile
+        ui.clearProfile();
     }
 
     
--- github.js
+++ github.js
@@ -2,15 +2,21 @@ class GitHub {
     constructor() {
         this.client_id = '12876354eda6f15c61ec';
         this.client_secret = '401c717104f055ac7026c4620d227e749a10f47e';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
 
         return {
-            profile
+            profile,
+            repos
         }
     }
 }
